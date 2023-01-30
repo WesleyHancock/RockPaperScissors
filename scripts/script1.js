1ebd955359cbb7b892a23d5739d63f9538cfe62d
WesleyHancock <wesleyhancock@protonmail.com>
2023-01-30 21:44:31 +0200

    06 - get computors input Translate computors random choice to 'rock', 'paper' or 'scissors'

--- scripts/script1.js
+++ scripts/script1.js
@@ -18,3 +18,16 @@ let usersChoice = function() {
     };
 };
 
+// get computors choice
+let compChoice = function() {
+    let random = Math.floor(Math.random() * 3);
+
+    if (random === 0) {
+        return 'rock';
+    } else if (random === 1) {
+        return 'paper';
+    } else if ( random === 2) {
+        return 'scissors';
+    };
+
+};
