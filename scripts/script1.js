61a62e8d4acadc73deaab02b860af9b3cf74da92
WesleyHancock <wesleyhancock@protonmail.com>
2023-02-15 13:36:25 +0200

    10 - added 5 rounds added in player and comp scores added tallying the scores remove old score and replace with new value after 5 rounds, reset the scores to 0

--- scripts/script1.js
+++ scripts/script1.js
@@ -9,28 +9,44 @@ let result = document.querySelector('.result');
 // GRAB ALL BUTTONS USER CAN SELECT
 let userOptions = document.querySelectorAll('.userOptions button');
 
+// VARIABLE FOR USERS CHOICE IN THE BUTTONS ARRAY
 let option;
 
 // DISPLAY USERS CHOICE BEFORE START OF GAME
 let displayClick = document.querySelector('.displayClick');
 
-
-
+// DISPLAY USERS CLICK AND SET VALUE OF OPTION TO BE LATER TRANSLATED
 userOptions[0].addEventListener('click', () => {
     option = 0;
     displayClick.textContent = `Rock is selected`;
+    return;
 })
 
 userOptions[1].addEventListener('click', () => {
     option = 1;
     displayClick.textContent = `Paper is selected`;
+    return;
 })
 
 userOptions[2].addEventListener('click', () => {
     option = 2;
     displayClick.textContent = `Scissors is selected`;
+    return;
 })
 
+// players score display <p>
+let usersTally = document.querySelector('.tally .usersTally');
+
+// players score number
+let playerTallyNumber = 0;
+
+// comps score display <p>
+let compTally = document.querySelector('.tally .compTally');
+
+// comps score number
+let compTallyNumber = 0;
+
+let tally;
 
 // BEGIN GAME FUNCTION
 function begin() {
@@ -40,6 +56,11 @@ function begin() {
 
     displayClick.textContent = '';
 
+    // RESET PLAYER SCORE AND UPDATE THE VALUE
+    usersTally.textContent = `Player score:`;
+
+    // RESET COMP SCORE AND UPDATE THE VALUE
+    compTally.textContent = `Comp score:`;
 
     // GET USERS CHOICE
     let usersChoice = function() {
@@ -98,6 +119,7 @@ function begin() {
             document.querySelector('.container').appendChild(button);
             button.className = 'resetButton';
             button.addEventListener('click', reset);
+            return button;
         }
 
         // RESET FUNCTION
@@ -105,18 +127,23 @@ function begin() {
             // RESET THE USERS SELECTION
             option = null;
 
-
             // RESET THE RESULTS DISPLAY
             result.textContent = '';
             result.style.backgroundColor = 'white';
             result.style.color = 'white';
 
+            // RESET PLAYER SCORE AND UPDATE THE VALUE
+            usersTally.textContent = `Player score: ${playerTallyNumber}`;
+
+            // RESET COMP SCORE AND UPDATE THE VALUE
+            compTally.textContent = `Comp score: ${compTallyNumber}`;
+
             // RESET THE COMPSELECTION
             compTranslate(random());
 
             // REMOVE THE RESET BUTTON
-            let remove = document.querySelector('.resetButton');
-            remove.remove();
+            let removeReset = document.querySelector('.resetButton');
+            removeReset.remove();
 
             // ENABLE STARTGAME BUTTON
             startGame.disabled = false;
@@ -125,6 +152,28 @@ function begin() {
 
         };
 
+        let scoreReset = function() {
+            playerTallyNumber = 0;
+            compTallyNumber = 0;
+        }
+
+        let fiveRounds = function() {
+            usersTally.append(playerTallyNumber);
+            compTally.append(compTallyNumber);
+
+            if (playerTallyNumber >= 5) {
+                result.textContent = `Your score is ${playerTallyNumber} and the Computors score is ${compTallyNumber} therefore you WIN it all!`;
+                // call score reset
+                scoreReset();
+            } else if (compTallyNumber >= 5) {
+                result.textContent = `Your score is ${playerTallyNumber} and the Computors score is ${compTallyNumber} therefore you LOSE it all!`;
+                // call score reset
+                scoreReset();
+            };
+
+            btn();
+        }
+
         // UNDEFINED FUNCTION
         let undefinedSituation = function() {
             result.textContent = `You have not selected a value.`;
@@ -132,40 +181,44 @@ function begin() {
             result.style.color = 'white';
             result.style.padding = '10px';
             result.style.borderRadius = '10px';
-            return btn();
+            return fiveRounds();
         }
 
         // DRAW FUNCTION
         let draw = function() {
             result.textContent = `You selected ${usersSelection} and the computor
-            selected ${compSelection}, therefore it is a DRAW`;
+            selected ${compSelection}, therefore it is a DRAW this round`;
             result.style.backgroundColor = 'black';
             result.style.color = 'white';
             result.style.padding = '10px';
             result.style.borderRadius = '10px';
-            return btn();
+            return fiveRounds();
         };
 
         // WIN FUNCTION
         let win = function() {
             result.textContent = `You selected ${usersSelection} and the computor
-            selected ${compSelection}, therefore you WIN!`;
+            selected ${compSelection}, therefore you WIN this round!`;
             result.style.backgroundColor = 'green';
             result.style.color = 'white';
             result.style.padding = '10px';
             result.style.borderRadius = '10px';
-            return btn();
+            // increment usersTally
+            playerTallyNumber += 1;
+            fiveRounds();
         }
 
         // LOSE FUNCTION
         let lose = function() {
             result.textContent = `You selected ${usersSelection} and the computor
-            selected ${compSelection}, therefore you LOSE!`;
+            selected ${compSelection}, therefore you LOSE this round!`;
             result.style.backgroundColor = 'red';
             result.style.color = 'white';
             result.style.padding = '10px';
             result.style.borderRadius = '10px';
-            return btn();
+            // increment compTally
+            compTallyNumber += 1;
+            return fiveRounds();
         }
 
         if (usersSelection === undefined || usersSelection === null) {
@@ -177,15 +230,16 @@ function begin() {
         } else if ((usersSelection === 'rock' && compSelection === 'scissors') || (usersSelection === 'paper' && compSelection === 'rock') || (usersSelection === 'scissors' && compSelection === 'paper')) {
             // console.log(`User: ${usersSelection} and Comp: ${compSelection}`);
             // console.log(`WIN`);
+            tally = 'win';
             return win();
         } else {
             // console.log(`User: ${usersSelection} and Comp: ${compSelection}`);
             // console.log(`LOSE`);
+            tally = 'lose';
             return lose();
         }
     }
 
     outcome();
-
 
 }
